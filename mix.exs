0975a945758d9ac404e84f57b128e1fad9b587e6
Teddy Koomen <teddy@Teddys-MBP.home>
2019-03-10 18:49:29 -0400

    fix typo

--- mix.exs
+++ mix.exs
@@ -23,7 +23,6 @@ defmodule Cards.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    { status, binary } =
     { :ex_doc, "~> 0.12" }
     ]
   end
